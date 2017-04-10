07d706b33e153118afeb004f721f93b0ebfd52a9
ilz2010 <ilz2010@yandex.ru>
2017-04-10 18:06:38 +0500

    Added THall render function FIX.1

--- db/migrate/20170403131042_create_tickets.rb
+++ db/migrate/20170403131042_create_tickets.rb
@@ -1,7 +1,7 @@
 class CreateTickets < ActiveRecord::Migration[5.0]
 	def change
 		create_table :tickets do |t|
-			t.belongs_to :user, foreign_key: true
+			t.belongs_to :u_api, foreign_key: true
 			t.belongs_to :poster, foreign_key: true
 			t.string :ticket
 
